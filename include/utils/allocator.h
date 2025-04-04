713446feb5ef521608009bb56078f2ea9e49de89
Vladimir Paskal <vp263@list.ru>
2025-04-04 12:02:00 +0300

    feat: added allocator traits

--- include/utils/allocator.h
+++ include/utils/allocator.h
@@ -6,6 +6,12 @@
  */
 
 #include <cstddef>
+#include <cstdio>
+#include <new>
+
+#include "utils/move.h"
+
+namespace bmb {
 
 /**
  * @class PrimitiveAllocator
@@ -15,8 +21,9 @@
  * calls new/delete
  */
 class PrimitiveAllocator {
+   public:
     /**
-     * @brief Allocate raw memory for n objects of type  T
+     * @brief Allocates raw memory for n objects of type  T
      *
      * @param n Number of objects to allocate
      * @return Pointer to the allocated memory
@@ -38,7 +45,6 @@ class PrimitiveAllocator {
      *
      * @param ptr Pointer to the previously allocated memory
      * @param _ Dummy argument
-     * @return Description of the returned value
      *
      * @throws noexcept
      */
@@ -57,5 +63,80 @@ class PrimitiveAllocator {
  * in case a allocator dosn't do it. Otherwise it calls
  * the allocator's method.
  */
-// template <typename T>
-// class AllocatorTraits {};
+template <typename Alloc>
+class AllocatorTraits {
+   public:
+    using allocator_type = Alloc;
+
+    /**
+     * @brief Forwards given arguments to allocator's construct method
+     * if present. Otherwise uses placement new
+     *
+     * @param alloc Allocator to use in construction
+     * @param ptr Pointer to raw memory where to construct
+     * @param args... Arguments to pass in constructor
+     *
+     * @throws Same as alloc.construct(...) if present. Otherwise
+     * same specification as new
+     */
+    template <typename T, typename... Args>
+    static void construct(Alloc& alloc, T* ptr, Args&&... args) {
+        // Use allocator if can
+        if constexpr (requires {
+                          alloc.construct(ptr, forward<Args>(args)...);
+                      }) {
+            alloc.construct(ptr, forward<Args>(args)...);
+        } else {
+            new (ptr) T(forward<Args>(args)...);
+        }
+    }
+
+    /**
+     * @brief Destroys object throgh alloctor if destroy method is present.
+     * Otherwise calls ~T()
+     *
+     * @param alloc Allocator to use in destruction
+     * @param ptr Pointer to object to destroy
+     *
+     * @throws If allocator has method destroy - throws the same.
+     * Otherwise doesn't throw(in assumption object destructor doesn't throw)
+     */
+    template <typename T>
+    static void destroy(Alloc& alloc, T* ptr) {
+        // Use allocator if can
+        if constexpr (requires { alloc.destroy(ptr); }) {
+            alloc.destroy(ptr);
+        } else ptr->~T();
+    }
+
+    /**
+     * @brief Allocates given number of objects using allocator
+     *
+     * @param alloc Allocator to use for allocation
+     * @param n Number of objects to allocate
+     *
+     * @return Pointer to the allocated memory
+     *
+     * @throws Same as alloc.allocate(...)
+     */
+    template <typename T>
+    static T* allocate(Alloc& alloc, size_t n) {
+        return alloc.template allocate<T>(n);
+    }
+
+    /**
+     * @brief Deallocates memory under ptr through allocator
+     *
+     * @param alloc Allocator to use in deallocation
+     * @param ptr Pointer to the previously allocated memory
+     * @param n Number of objects to deallocate
+     *
+     * @throws Same specification as alloc.deallocate()
+     */
+    template <typename T>
+    static void deallocate(Alloc& alloc, T* ptr, size_t n) {
+        alloc.deallocate(ptr, n);
+    }
+};
+
+}  // namespace bmb
