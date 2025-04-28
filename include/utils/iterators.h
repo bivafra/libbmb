429c6a034a877f08b63dd6504577f5a9d5c8ce1a
Vladimir Paskal <vp263@list.ru>
2025-04-28 16:42:01 +0300

    feat: added general iterator operations

--- include/utils/iterators.h
+++ include/utils/iterators.h
@@ -102,4 +102,110 @@ concept RandomAccessIterator
 template <typename Iter>
 concept ContiguousIterator = RandomAccessIterator<Iter>;
 
+/**
+ * @brief Returns the number of elements in [first, last).
+ *
+ * Time complexity: O(1) if Iter satisfies at least RandomAccessIterator,
+ * otherwise O(last - first).
+ *
+ * @param first Iterator to the first element
+ * @param last Iterator to the end of the range
+ *
+ * @return The number of increments needed to go from first to last
+ *
+ * @throws Almost always nothrow. In general, the same as
+ * either operator-(It, It) or operator++()
+ */
+template <InputIterator Iter>
+auto distance(Iter first, Iter last) -> IteratorTraits<Iter>::difference_type {
+    using diff_t        = IteratorTraits<Iter>::difference_type;
+    using iter_category = IteratorTraits<Iter>::iterator_category;
+
+    // If can - do it for O(1), otherwise iterate through all range
+    if constexpr (is_base_of_v<random_access_iter_tag, iter_category>) {
+        return last - first;
+    }
+
+    diff_t n = 0;
+    while (first != last) {
+        ++first;
+        ++n;
+    }
+    return n;
+}
+
+/**
+ * @brief Increments given iterator by dist.
+ *
+ * If iterator is at least RandomAccessIterator - uses +=.
+ * If it BidirectionalIterator - uses either + or - dist times.
+ * If it at least InputIterator and dist > 0 - uses +, if dist < 0 - has no effect.
+ *
+ * Time complexity: O(1) for RandomAccessIterator, otherwise O(dist)
+ *
+ * @param it Iterator to be advanced
+ * @param dist Number of elements iterator should be advanced
+ *
+ * @throws Almost always nothrow. In general, the same as
+ * modifying iterator operations.
+ */
+template <InputIterator Iter, typename Distance>
+void advance(Iter& it, Distance dist) {
+    using diff_t        = IteratorTraits<Iter>::difference_type;
+    using iter_category = IteratorTraits<Iter>::iterator_category;
+
+    auto n = static_cast<diff_t>(dist);
+
+    // Make operations according to the iterator category
+    if constexpr (is_base_of_v<random_access_iter_tag, iter_category>)
+        it += n;
+
+    else {
+        while (n > 0) {
+            --n;
+            ++it;
+        }
+
+        if constexpr (is_base_of_v<bidirectional_iter_tag, iter_category>) {
+            while (n < 0) {
+                ++n;
+                --it;
+            }
+        }
+    }
+}
+
+/**
+ * @brief Returns the n-th successor of given iterator.
+ *
+ * @param it Iterator
+ * @param n Number of elements to shift the iterator
+ *
+ * @return Iterator that holds n-th successor of the given one
+ *
+ * @throws Same as advance(Iter, Distance)
+ */
+template <InputIterator Iter>
+Iter next(Iter it, typename IteratorTraits<Iter>::difference_type n = 1) {
+    advance(it, n);
+    return it;
+}
+
+/**
+ * @brief Returns the n-th predecessor of given iterator.
+ * If n < 0, will chose n-th successor.
+ *
+ * @param it Iterator
+ * @param n Number of elements to shift the iterator
+ *
+ * @return Iterator that holds n-th predecessor of the given one
+ *
+ * @throws Same as advance(Iter, Distance)
+ */
+template <BidirectionalIterator Iter, typename Distance>
+Iter prev(Iter it, typename IteratorTraits<Iter>::difference_type n = 1) {
+    advance(it, -n);
+    return it;
+}
+
 }  // namespace bmb
