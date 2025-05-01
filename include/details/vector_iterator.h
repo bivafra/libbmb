528d9122c80f9b4cf6b36527223d36353b6e430f
Vladimir Paskal <vp263@list.ru>
2025-05-01 17:15:04 +0300

    fix: vector iterator operator- recursive call

--- include/details/vector_iterator.h
+++ include/details/vector_iterator.h
@@ -86,7 +86,7 @@ public:
     Self operator-(difference_type n) { return {ptr_ - n}; }
 
     friend difference_type operator-(const Self& lhs,
-                                     const Self& rhs) { return lhs - rhs; }
+                                     const Self& rhs) { return lhs.ptr_ - rhs.ptr_; }
 
     // NOTE: consider spaceship opeator
     bool operator<(const Self& rhs) { return ptr_ < rhs.ptr_; }
